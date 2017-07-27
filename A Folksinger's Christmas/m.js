8ac43133fa35069b9e7daef31c0c8b952df34c7c
littleflute <13699175164@sina.cn>
2017-07-27 18:02:26 -0500

    Add files via upload

---
+++ A Folksinger's Christmas/m.js
@@ -0,0 +1,16 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Virgin Mary Had One Son.mp3";		
+f[2]	= "02 Mary's Wandering.mp3";		
+f[3]	= "03 Poor Little Jesus.mp3";		
+f[4]	= "04 Silent Night.mp3";		
+f[5]	= "05 Santa Claus Is Coming (It's Almost Day).mp3";		
+f[6]	= "06 Children, Go Where I Send Thee.mp3";		
+f[7]	= "07 Cantique de Noel [Sung in French].mp3";		
+f[8]	= "08 Go Tell It on the Mountain.mp3";		
+f[9]	= "09 Virgin Mary.mp3";		
+f[10]	= "10 Little Drummer Boy.mp3";		
+f[11]	= "11 Esta Navidad [Sung in Spanish].mp3";		
+f[12]	= "12 What Month Was Jesus Born In-.mp3";		
+f[13]	= "13 Away in a Manger.mp3";		
+f[14]	= "14 We Wish You a Merry Christmas.mp3";		
